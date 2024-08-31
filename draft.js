68c004c0b027b22472188698dff32816e55e7d65
mizansanzi <pleasemailmizan@gmail.com>
2024-08-31 17:18:15 +0600

    first commit

---
+++ draft.js
@@ -0,0 +1,16 @@
+// division
+var first = 45;
+var second = 3;
+var result = first/second;
+console.log (result);
+
+// modulo 
+
+var one = 5;
+var two = 2;
+var modulo = one%two;
+console.log (modulo);
+
+let x = "10";
+let num = Number(x);
+console.log (num);
---
+++ task1.js
@@ -0,0 +1,27 @@
+// Module 17 
+
+// task 1
+
+var totalGivenMoney = 1000;
+var orangeAndApplePrice = 300 + 400;
+var output =totalGivenMoney-orangeAndApplePrice;
+console.log(output);
+
+// task 2
+
+var math = 75.25;
+var biology = 65;
+var chemi = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var total = math+biology+chemi+physics+bangla;
+var average = total/5;
+console.log(average.toFixed(2));
+
+// task 3
+
+var first = 119;
+var second = 5;
+var modulo = first%second;
+console.log (modulo);
+
